79baa3fcf7e685ac50842eaecaaa4db1a21b7f2f
samuelitoprogaminDJ <vjfcytjhbm@privaterelay.appleid.com>
2026-07-09 16:30:36 -0500

    Agregar ejercicios de Python

---
+++ reto matematico1.py
@@ -0,0 +1,6 @@
+#define n1,n2,n3 promedio
+n1=float (input("ingrese nota 1 :"))
+n2=float (input("ingrese nota 2 :"))
+n3=float (input("ingrese nota 3 :"))
+promedio=(n1+n2+n3)/3
+print("El resultado de la nota final es"),promedio
