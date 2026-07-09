65df6099f8d7c68bc161c78c65027976172e96f9
samuelitoprogaminDJ <vjfcytjhbm@privaterelay.appleid.com>
2026-07-09 16:36:55 -0500

    ejercicio de paython

---
+++ .py
@@ -0,0 +1,9 @@
+print("SOY EL MJEOR ESTUDIANTE Y JUGADOR DE TENIS DE MESA")
+#definir el perimetro de un cuadro
+l1=8
+l2=6
+suma=l1+l2
+print(suma)
+lado=6
+perimetro = 4*lado
+print("el perimetro es :", perimetro)
